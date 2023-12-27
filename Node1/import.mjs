f689367d78801771f99b3a5db47ae28d7653918d
Aniket <aniketraj28042003@gmail.com>
2023-12-27 18:38:10 +0530

    added node codes

--- Node1/import.mjs
+++ Node1/import.mjs
@@ -1,5 +1,7 @@
-import searching, { linear } from './modeule_export.js';
-let arr = [1,2,3,4,5,6,7,8,9,10];
-console.log(searching.linear(arr, 8));
-console.log(searching.bianry(arr, 8));
-console.log(searching);
+// import searching, { linear } from './modeule_export.js';
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(searching.linear(arr, 8));
+// console.log(searching.bianry(arr, 8));
+// console.log(searching);
+// console.log(__dirname);
+console.log(import.meta.url);
---
+++ Node2/shebang.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+console.log("shebang executed");
+console.log(process.argv);
