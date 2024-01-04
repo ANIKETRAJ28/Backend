d6419b227780ca3d3fd380c594d025cf6066e7f1
Aniket <aniketraj28042003@gmail.com>
2024-01-04 19:48:25 +0530

    added codes for restful apis

--- 7_Restful_Api/index.js
+++ 7_Restful_Api/index.js
@@ -1,11 +1,44 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(PORT, ()=>{
     console.log("server running at", PORT);
 });
+// we will try to mimic the database using an array
+let blogList = [];
 
-app.get("/", ()=>{
-    console.log("hello");
+app.get("/blogs", (req, res) =>{
+    return res.status(200).json({
+        data : blogList,
+        success : true,
+    });
+});
+
+app.post("/blogs", (req, res) =>{
+    blogList.push({title: req.body.title,
+        content: req.body.content,
+        id: Math.floor(Math.random()*1000)
+    });
+    return res.status(201).json({
+        success: true
+    });
+});
+
+app.get("/blogs/:id", (req,res) =>{
+    const result = blogList.filter((blog) => blog.id == req.params.id);
+    return res.status(201).json({
+        data : result,
+        success: true
+    });
+});
+app.delete("/blogs/:id", (req,res) =>{
+    blogList = blogList.filter((blog) => blog.id != req.params.id);
+    return res.status(201).json({
+        success: true
+    });
 });
